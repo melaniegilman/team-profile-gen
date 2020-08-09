3f2e629274e798c77ffb602218832bc5cb127b68
Melanie Gilman <melaniegilman259@yahoo.com>
2020-08-09 14:56:39 -0400

    tests working but modules not found

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,10 @@
 const Manager = require('../lib/Manager');
-const { TestScheduler } = require('jest');
+// const { TestScheduler } = require('jest');
 
 test('checks Manager office number', () => {
     const manager = new Manager('John', 1234, 'john@gmail.com', 24);
 
-    expect(manager.office).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 test('check getRole to return Manager', () => {
